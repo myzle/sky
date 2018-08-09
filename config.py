e6a505cd1f3072be3f4e40d387b7de775fba34ef
Myzle <myzle@yeah.net>
2018-08-09 17:58:24 +0800

    update send email method

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ monitor_api_task = [
                                     0007-初始化控制 0008-首页 0009-我的页面 0010-转账页面预加载信息 0011-一个游戏
                                     0012-秘钥约定接口 0013-获取语言配置接口 0014-币种列表 0015-币种添加自选
                                     0016-币种删除自选 0017-币种重设自选 0018-发现页 0019-获取cms配置内容接口 0020-搜索token列表'''
-    },
+     },
 ]
 monitor_circle = 1200
 send_sms_phones_self = '18500197001'
@@ -14,3 +14,8 @@ send_sms_phones = '18500197001'
 
 login_timeout = 3000
 timeout = 3000
+
+from_address = 'myzle@yeah.net'
+key_word = 'Hb2:)sophie'
+to_address = 'myzle@yeah.net'
+smtp_server = 'smtp.yeah.net'
--- send_mail.py
+++ send_mail.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+"""
+This module is fro sending email with python
+"""
+
+import smtplib
+import config
 from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart, MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.utils import parseaddr, formataddr
-import smtplib
 
 
 def _format_address(s):
+    """format email address for use"""
     name, address = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), address))
 
 
 def send_email(from_address, password, to_address, smtp_server):
+    """send email with html"""
     msg = MIMEText('<html><body><h1>Hello</h1>' + '<p>send by <a href="http://www.python.org">Python</a>...</p>' +
                    '</body></html>', 'html', 'utf-8')
-    msg['From'] = _format_address('Python爱好者 <%s>' % from_address)
+    msg['From'] = _format_address('接口测试报告 <%s>' % from_address)
     msg['To'] = _format_address('管理员 <%s>' % to_address)
-    msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+    msg['Subject'] = Header('接口测试报告', 'utf-8').encode()
 
     server = smtplib.SMTP(smtp_server, 25)
     server.set_debuglevel(1)
@@ -29,14 +38,16 @@ def send_email(from_address, password, to_address, smtp_server):
 
 
 def send_email_with_attachment(from_address, password, to_address, smtp_server):
+    """send email with attachment(picture) and show it directly"""
     # 邮件对象:
     msg = MIMEMultipart()
-    msg['From'] = _format_address('Python爱好者 <%s>' % from_address)
+    msg['From'] = _format_address('tester <%s>' % from_address)
     msg['To'] = _format_address('管理员 <%s>' % to_address)
-    msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+    msg['Subject'] = Header('接口测试报告', 'utf-8').encode()
 
     # 邮件正文是MIMEText:
-    msg.attach(MIMEText('<html><body><h1>Hello</h1>' + '<p><img src="cid:0"></p>' + '</body></html>', 'html', 'utf-8'))
+    msg.attach(MIMEText('<html><body><h1>API status</h1>' + '<p><img src="cid:0"></p>' + '</body></html>',
+                        'html', 'utf-8'))
 
     # 添加附件就是加上一个MIMEBase，从本地读取一个图片:
     with open('/Users/myzle/desktop/1.jpg', 'rb') as f:
@@ -62,5 +73,5 @@ def send_email_with_attachment(from_address, password, to_address, smtp_server):
 
 if __name__ == '__main__':
 
-    # send_email('myzle@yeah.net', 'Hb2:)sophie', 'myzle@yeah.net', 'smtp.yeah.net')
-    send_email_with_attachment('myzle@yeah.net', 'Hb2:)sophie', 'myzle@yeah.net', 'smtp.yeah.net')
+    # send_email(config.from_address, config.key_word, config.to_address, config.smtp_server)
+    send_email_with_attachment(config.from_address, config.key_word, config.to_address, config.smtp_server)
